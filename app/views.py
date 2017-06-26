c3ca5f041569db8fdb2f518eb16e056d2fb2a338
chlv <lc923@live.cn>
2017-06-27 02:54:25 +0800

    动态路由和登陆页面

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 # --coding:utf-8 --
 from app import app
-from flask import render_template
+from flask import render_template,request
 
 @app.route("/")
 @app.route("/index")
@@ -9,3 +9,27 @@ def index():
 	posts = [{"author":{"nickname":"John"},"body":"beautiful day in portland"},
 			 {"author":{"nickname":"Susan"},"body":"The Avengers movie was so cool"}]
 	return render_template("index.html",title="Home",user=user,posts=posts)
+
+
+@app.route("/services")
+def services():
+	return "Services"
+
+@app.route("/about")
+@app.route("/info")
+def about():
+	return "About"
+
+@app.route("/user/<username>")
+def user(username):
+	return "username %s" % username
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+	if request.method == "POST":
+		username=request.form["username"]
+		password=request.form["password"]
+		return render_template("login.html",method=request.method)
+	else:
+		return render_template("login.html",method=request.method)
+
